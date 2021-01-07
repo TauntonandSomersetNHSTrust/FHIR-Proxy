b582347812372fb8bf0a7984839521624c4a2f9e
NHS-juju <39803323+JMNHS@users.noreply.github.com>
2021-01-07 11:48:51 +0000

    Encounter participant logic rewritten to evaluate classcode and split the actions based on this.

--- src/Mirth Code Templates/Build Encounter Resource.js
+++ src/Mirth Code Templates/Build Encounter Resource.js
@@ -25,7 +25,7 @@ function buildEncounterResource(data) {
 	resource.id = newStringOrUndefined(result.Encounter_ID);
 	resource.status = newStringOrUndefined(result.status);
 
-	// Add meta data
+	// Only add a valid date to metadata
 	if (
 		result.Last_Updated &&
 		result.Last_Updated.substring(0, 1) != 'T' &&
@@ -34,19 +34,12 @@ function buildEncounterResource(data) {
 		resource.meta.lastUpdated = result.Last_Updated;
 	}
 
-	if (result.className) {
-		resource.class = {
-			system: 'https://hl7.org/fhir/v3/ActEncounterCode',
-			code: newStringOrUndefined(result.classCode),
-			display: newStringOrUndefined(result.className)
-		};
-	}
-
 	/**
-	 * Add SIDeR specific tags
+	 * Add SIDeR specific tags to metadata
 	 * Only display in summary and detail view if within last 30 days,
 	 * don't display any planned/future encounters as they're out of scope
 	 */
+
 	if (
 		result.Start_Date &&
 		result.Start_Date.substring(0, 1) != 'T' &&
@@ -88,7 +81,16 @@ function buildEncounterResource(data) {
 			}
 		];
 	}
-
+	
+	if (result.className) {
+			resource.class = {
+				system: 'https://hl7.org/fhir/v3/ActEncounterCode',
+				code: newStringOrUndefined(result.classCode),
+				display: newStringOrUndefined(result.className)
+			};
+		}
+	
+	// Add type element content
 	resource.type = [];
 
 	const emptyType = {
@@ -101,10 +103,12 @@ function buildEncounterResource(data) {
 		]
 	};
 
-	////
-	//Array structure should be:
-	//system,code,display|system,code,display|system,code,display
-	////
+	/**
+	* result.encountertype is provided as an array to be processed and applied to the resource.type array.
+	* Array structure is:
+	* system,code,display|system,code,display|system,code,display
+	*/
+
 	if (
 		result.encountertype
 	) {
@@ -130,6 +134,12 @@ function buildEncounterResource(data) {
 			resource.type.push(EncounterTypeEntry);
 		}
 	} else {
+		
+		/**
+		 * The else contains the legacy way of building up the resource.type elements.
+		 * This was based on the source data only providing the treatment function code for the specialty.
+		 */
+		
 		const primitiveEncounterType = JSON.parse(JSON.stringify(emptyType));
 		if (result.Service_Code) {
 			primitiveEncounterType.coding[0].system = 'https://fhir.nhs.uk/STU3/CodeSystem/Specialty-1'
@@ -147,120 +157,114 @@ function buildEncounterResource(data) {
 	// Add participants
 	resource.participant = [];
 	
- 	if (
-		result.encounterParticipantIndividualCode_admitting &&
-		result.encounterParticipantIndividualCode_discharging &&
-		result.encounterParticipantIndividualCode_discharging ==
-			result.encounterParticipantIndividualCode_admitting
-	) {
-		const participantCombo = {
-			type: [
-				{
-					coding: [
-						{
-							system: 'https://hl7.org/fhir/v3/ParticipationType',
+	// Inpatient paticipants
+	if(
+		result.classCode == 'IMP'
+		)
+	{
+		// If the same person admitted and discharged, build participantCombo
+		if(
+			result.disHCP == result.admHCP 
+			) 
+		{
+			const participantCombo = {
+				type: [{
+					coding: [{
+						system: 'https://www.hl7.org/fhir/valueset-encounter-participant-type.html',
+						code: 'ADM',
+						display: 'admitter'
+					}]
+				}, {
+					coding: [{
+						system: 'https://www.hl7.org/fhir/valueset-encounter-participant-type.html',
+						code: 'DIS',
+						display: 'discharger'
+					}]
+				}],
+				individual: {
+					identifier: {
+						value: result.encounterParticipantIndividualCode_admitting
+					},
+					display: result.admHCP
+				}
+			};
+			resource.participant.push(participantCombo);
+		}
+		// otherwise, add individually
+		else {
+			if(result.admHCP) {
+				const participantAdmitter = {
+					type: [{
+						coding: [{
+							system: 'https://www.hl7.org/fhir/valueset-encounter-participant-type.html',
 							code: 'ADM',
 							display: 'admitter'
-						}
-					]
-				},
-				{
-					coding: [
-						{
-							system: 'https://hl7.org/fhir/v3/ParticipationType',
+						}]
+					}],
+					individual: {
+						identifier: {
+							value: result.encounterParticipantIndividualCode_admitting
+						},
+						display: result.admHCP
+					}
+				};
+				resource.participant.push(participantAdmitter);
+			}
+			if(result.disHCP) {
+				const participantDischarger = {
+					type: [{
+						coding: [{
+							system: 'https://www.hl7.org/fhir/valueset-encounter-participant-type.html',
 							code: 'DIS',
 							display: 'discharger'
-						}
-					]
-				}
-			],
-			individual: {
-				identifier: {
-					value: result.encounterParticipantIndividualCode_admitting
-				},
-
-				display: result.encounterParticipantIndividualDisplay_admitting
-			}
-		};
-		resource.participant.push(participantCombo);
-	}
-
-	if (resource.participant.length === 0) {
-		if (result.encounterParticipantIndividualCode_admitting ) {
-			const participantAdmitter = {
-				type: [
-					{
-						coding: [
-							{
-								system:
-									'https://hl7.org/fhir/v3/ParticipationType',
-								code: 'ADM',
-								display: 'admitter'
-							}
-						]
+						}]
+					}],
+					individual: {
+						identifier: {
+							value: result.encounterParticipantIndividualCode_discharging
+						},
+						display: result.disHCP
 					}
-				],
+				};
+				resource.participant.push(participantDischarger);
+			}
+		}
+	} 
+	
+	// Outpatient participant
+	else if(
+		result.classCode == 'AMB'
+		) 
+	{
+		if(result.seenBy) {
+			const participantConsultant = {
+				type: [{
+					coding: [{
+						system: 'https://www.hl7.org/fhir/valueset-encounter-participant-type.html',
+						code: 'CON',
+						display: 'consultant'
+					}]
+				}],
 				individual: {
-					identifier: {
-						value:
-							result.encounterParticipantIndividualCode_admitting
-					},
-					display:
-						result.encounterParticipantIndividualDisplay_admitting
+					identifier: result.encounterParticipantIndividualCode_opattending,
+					display: result.seenBy
 				}
 			};
-			resource.participant.push(participantAdmitter);
+			resource.participant.push(participantConsultant);
 		}
-		if (
-			result.encounterParticipantIndividualCode_discharging
-		) {
-			const participantDischarger = {
-				type: [
-					{
-						coding: [
-							{
-								system:
-									'https://hl7.org/fhir/v3/ParticipationType',
-								code: 'DIS',
-								display: 'discharger'
-							}
-						]
-					}
-				],
-				individual: {
-					identifier: {
-						value:
-							result.encounterParticipantIndividualCode_discharging
-					},
-					display:
-						result.encounterParticipantIndividualDisplay_discharging
-				}
-			};
-			resource.participant.push(participantDischarger);
-		}
-	}
-	if (result.encounterParticipantIndividualCode_opattending ) {
-		const participantConsultant = {
-			type: [
-				{
-					coding: [
-						{
-							system: 'https://hl7.org/fhir/v3/ParticipationType',
-							code: 'CON',
-							display: 'consultant'
-						}
-					]
-				}
-			],
-			individual: {
-				identifier:
-					result.encounterParticipantIndividualCode_opattending,
-				display:
-					result.encounterParticipantIndividualDisplay_opattending
-			}
-		};
-		resource.participant.push(participantConsultant);
-	}
+	} 
+	// A&E Participant
+	else if(
+		result.classCode == 'EMER'
+		) 
+	{
+		/**
+		 * TODO
+		 * 
+		 */
+	} 
+	// Unexpected code returned, add no participants
+	else {} 
 
 	resource.period = {};
 	if (
@@ -279,140 +283,59 @@ function buildEncounterResource(data) {
 	}
 
 	// Add admission and discharge inpatient details
-	resource.hospitalization = {};
-
-	if (
-		result.encounterAdmissionmethodCodingCode ||
-		result.encounterDischargemethodCodingCode 
+	if(
+		result.classCode == 'IMP'
 	) {
-		resource.hospitalization.extension = [];
-	}
-
-	if (result.encounterAdmissionmethodCodingCode ) {
-		const admissionMethod = {
-			url:
-				'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-AdmissionMethod-1',
-			valueCodeableConcept: {
-				coding: [
-					{
-						system:
-							'https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-AdmissionMethod-1',
-						code: result.encounterAdmissionmethodCodingCode,
-						display: newStringOrUndefined(
-							result.encounterAdmissionmethodCodingDesc
-						)
-					}
-				]
-			}
-		};
-		resource.hospitalization.extension.push(admissionMethod);
-	}
-
-	if (result.encounterDischargemethodCodingCode ) {
-		const dischargeMethod = {
-			url:
-				'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-DischargeMethod-1',
-			valueCodeableConcept: {
-				coding: [
-					{
-						system:
-							'https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-DischargeMethod-1',
-						code: result.encounterDischargemethodCodingCode,
-						display: newStringOrUndefined(
-							result.encounterDischargemethodCodingDesc
-						)
-					}
-				]
-			}
-		};
-		resource.hospitalization.extension.push(dischargeMethod);
-	}
-
-	if (result.encounterHospitalizationAdmitsourceCodingCode ) {
-		resource.hospitalization.admitSource = {
-			coding: [
-				{
-					system:
-						'https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-SourceOfAdmission-1',
-					code: result.encounterHospitalizationAdmitsourceCodingCode,
-					display: newStringOrUndefined(
-						result.encounterHospitalizationAdmitsourceCodingDesc
-					)
-				}
-			]
-		};
-	}
-	if (
-		result.encounterHospitalizationDischargedispositionCodingCode !==
-		undefined
-	) {
-		resource.hospitalization.dischargeDisposition = {
-			coding: [
-				{
-					system:
-						'https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-DischargeDestination-1',
-					code:
-						result.encounterHospitalizationDischargedispositionCodingCode,
-					display: newStringOrUndefined(
-						result.encounterHospitalizationDischargedispositionCodingDesc
-					)
-				}
-			]
-		};
-	}
-
-	// Add location details
-	if (
-		result.encounterClassCode &&
-		result.encounterClassCode == 'XXIMP'
-	) {
-		resource.location = [];
-
-		const emptyLocation = {
-			location: {
-				identifier: {
-					value: undefined
-				},
-				display: undefined
-			},
-			period: {
-				start: undefined,
-				end: undefined
-			}
-		};
-
-		if (
-			result.encounterLocation1Identifier &&
-			typeof resource.period.start !== 'undefined'
+		resource.hospitalization = {};
+		
+		if(
+			result.encounterAdmissionmethodCodingCode || result.encounterDischargemethodCodingCode
 		) {
-			const admittingWard = JSON.parse(JSON.stringify(emptyLocation));
-
-			admittingWard.location.identifier.value = newStringOrUndefined(
-				result.encounterLocation1Identifier
-			);
-			admittingWard.location.display = newStringOrUndefined(
-				result.encounterLocation1Display
-			);
-
-			admittingWard.period.start = resource.period.start;
-			resource.location.push(admittingWard);
+			resource.hospitalization.extension = [];
 		}
-
-		if (
-			result.encounterLocation2Identifier &&
-			typeof resource.period.end !== 'undefined'
-		) {
-			const dischargeWard = JSON.parse(JSON.stringify(emptyLocation));
-
-			dischargeWard.location.identifier.value = newStringOrUndefined(
-				result.encounterLocation2Identifier
-			);
-			dischargeWard.location.display = newStringOrUndefined(
-				result.encounterLocation2Display
-			);
-
-			dischargeWard.period.end = resource.period.end;
-			resource.location.push(dischargeWard);
+		if(result.encounterAdmissionmethodCodingCode) {
+			const admissionMethod = {
+				url: 'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-AdmissionMethod-1',
+				valueCodeableConcept: {
+					coding: [{
+						system: 'https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-AdmissionMethod-1',
+						code: result.encounterAdmissionmethodCodingCode,
+						display: newStringOrUndefined(result.encounterAdmissionmethodCodingDesc)
+					}]
+				}
+			};
+			resource.hospitalization.extension.push(admissionMethod);
+		}
+		if(result.encounterDischargemethodCodingCode) {
+			const dischargeMethod = {
+				url: 'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-DischargeMethod-1',
+				valueCodeableConcept: {
+					coding: [{
+						system: 'https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-DischargeMethod-1',
+						code: result.encounterDischargemethodCodingCode,
+						display: newStringOrUndefined(result.encounterDischargemethodCodingDesc)
+					}]
+				}
+			};
+			resource.hospitalization.extension.push(dischargeMethod);
+		}
+		if(result.encounterHospitalizationAdmitsourceCodingCode) {
+			resource.hospitalization.admitSource = {
+				coding: [{
+					system: 'https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-SourceOfAdmission-1',
+					code: result.encounterHospitalizationAdmitsourceCodingCode,
+					display: newStringOrUndefined(result.encounterHospitalizationAdmitsourceCodingDesc)
+				}]
+			};
+		}
+		if(result.encounterHospitalizationDischargedispositionCodingCode !== undefined) {
+			resource.hospitalization.dischargeDisposition = {
+				coding: [{
+					system: 'https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-DischargeDestination-1',
+					code: result.encounterHospitalizationDischargedispositionCodingCode,
+					display: newStringOrUndefined(result.encounterHospitalizationDischargedispositionCodingDesc)
+				}]
+			};
 		}
 	}
 
